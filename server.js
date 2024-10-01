c2906e281fadf9250fd3acc71040596e033dbbb2
Iyanu Dayisi <tobiy23@gmail.com>
2024-10-01 17:17:02 +0100

    fix routes

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-import {getRoute, getMiddleware} from './routes';
-import Middleware from './lib/middlewares';
+import { getRoute } from './routes';
 
 const express = require('express');
 
 const app = express();
-const router = express.Router();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '200mb' }));
-router.use(Middleware.authMiddleware);
 
 getRoute(app);
-getMiddleware(router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
