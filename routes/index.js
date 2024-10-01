9245008bc230a5609f6c5b9e7f9a2db1d3e57f06
Iyanu Dayisi <tobiy23@gmail.com>
2024-10-01 18:25:43 +0100

    fix routes and auth

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-const getRoute = (app) => {
+const getRoute = (app, router) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
+  app.get('/connect', AuthController.getConnect);
+  app.get('/disconnect', AuthController.getDisconnect);
+  router.get('/users/me', UsersController.getMe);
 };
 
 export default getRoute;
