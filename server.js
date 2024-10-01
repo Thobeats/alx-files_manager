d9af859e424394304c31f199e5a1f7eb0262b90d
Iyanu Dayisi <tobiy23@gmail.com>
2024-10-01 17:26:21 +0100

    fix routes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-import { getRoute } from './routes';
+import getRoute from './routes';
+import Middleware from './lib/middlewares';
 
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '200mb' }));
+router.use(Middleware.authMiddleware);
 
 getRoute(app);
 
