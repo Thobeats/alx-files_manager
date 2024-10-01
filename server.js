9ffb43b0071de757c91e03a3e72a28373a8c4ae3
Iyanu Dayisi <tobiy23@gmail.com>
2024-10-01 18:33:13 +0100

    fix routes and auth

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import getRoute from './routes';
+import Middleware from './lib/middlewares';
 
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '200mb' }));
+router.use(Middleware.authMiddleware);
 
-getRoute(app);
+getRoute(app, router);
+
+app.use('', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
