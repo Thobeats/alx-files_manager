7b2c5fec6a175531df8733254ac2de56f7f557a7
Iyanu Dayisi <tobiy23@gmail.com>
2024-10-02 09:30:59 +0100

    update routes

--- routes/index copy.js
+++ routes/index copy.js
@@ -1,19 +1,22 @@
-//import multipart from 'connect-multiparty';
+import multipart from 'connect-multiparty';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const getRoute = (app, router) => {
+const getRoute = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
+ // router.get('/users/me', UsersController.getMe);
   app.get('/connect', AuthController.getConnect);
   app.get('/disconnect', AuthController.getDisconnect);
-  router.get('/users/me', UsersController.getMe);
-  // router.post('/files', multipart(), FilesController.postUpload);
-  // router.get('/files/:id', multipart(), FilesController.getShow);
-  // router.get('/files', multipart(), FilesController.getIndex);
+
+  router.post('/files', multipart(), FilesController.postUpload);
+ router.get('/files/:id', multipart(), FilesController.getShow);
+  router.get('/files', multipart(), FilesController.getIndex);
+
+//  app.use('', router);
 };
 
 export default getRoute;
